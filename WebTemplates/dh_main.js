a47103ea432015d9dfab83704d7f851565e6a8bf
Mahesh Bhalke <mahesh.bhalke@sarathisoftech.com>
2019-12-24 15:33:11 +0530

    i added new edited files with proper css and js and with proper naming convension

--- WebTemplates/dh_main.js
+++ WebTemplates/dh_main.js
@@ -2,41 +2,24 @@
 		document.getElementById("menuSidebar").style.width = "250px";
 		document.getElementById("bt-menu").style.marginLeft = "0px";
 		document.getElementById("menuSidebar").style.marginTop = "80px";
-		document.getElementById("maindiv").style.marginLeft = "270px";
+		document.getElementById("containder").style.marginLeft = "270px";
 	}
 
 	function closeMenu() {
 		document.getElementById("menuSidebar").style.width = "0";
 		document.getElementById("bt-menu").style.marginLeft = "0";
-		document.getElementById("maindiv").style.marginLeft = "30px";
+		document.getElementById("containder").style.marginLeft = "30px";
 	}
-	
-	$(document).ready(function() {
-		$("#defaultOpen").click(function() {
-			
-		});
+	  
+        var content_data = document.getElementById("tab-content");  
+              
+        document.getElementById('tab-1').onclick = tab_click; 
+        document.getElementById('tab-2').onclick = tab_click; 
+          
+              
+        function tab_click(clicked) { 
+        content_data.innerHTML = "Selected Button id = "+ this.id; 
+        }  
+	    
 
-		$("#defaultOpen1").click(function() {
-			
-		});
-	});
-	
-	function openLink1(evt, cityName) {
-		var i, tabcontent, tablinks;
-		tabcontent = document.getElementsByClassName("tabcontent");
-		for (i = 0; i < tabcontent.length; i++) {
-			tabcontent[i].style.display = "none";
-		}
-		tablinks = document.getElementsByClassName("tablinks");
-		for (i = 0; i < tablinks.length; i++) {
-			tablinks[i].className = tablinks[i].className
-					.replace(" active", "");
-		}
-		document.getElementById(cityName).style.display = "block";
-		evt.currentTarget.className += " active";
-	}
 
-	// Get the element with id="defaultOpen" and click on it
-	document.getElementById("defaultOpen").click();
-	document.getElementById("defaultOpen1").click();
-	document.getElementById("defaultOpen2").click();
